e1f17f66c92689d4e330ded34ef0e2e563bf41eb
David-steve <60090502+David-steve@users.noreply.github.com>
2020-09-15 13:37:52 +0800

    Update test_gpio.sh

--- shell/test_gpio.sh
+++ shell/test_gpio.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-declare -i value=$3
-for((i=$1;i<$2;i++));
+declare -i value=$3     //将参数3转为整数
+for((i=$1;i<$2;i++));   //需要有两对括号
 do
-echo `./gpio_test $i out $value`;
+./gpio_test $i out $value;
 echo "GPIO$i value:$value"
 done
